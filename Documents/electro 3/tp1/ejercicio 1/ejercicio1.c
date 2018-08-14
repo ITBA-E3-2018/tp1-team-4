3b40d20e5ccc71a5769c8a20615b1c1e4d672426
matias <mfogg@itba.edu.ar>
2018-08-14 19:03:42 -0300

    rough input idea

--- Documents/electro 3/tp1/ejercicio 1/ejercicio1.c
+++ Documents/electro 3/tp1/ejercicio 1/ejercicio1.c
@@ -1,6 +1,49 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <ctype.h>
+#define SUCCESS 0
 #define ERROR 1
-int main(int argc, char *argv[]){
 
+int main(){
+    bool status=ERROR;
+    int temp;
+    bool isSigned;
+    int wholeDigits;
+    int fractionaryDigits;
+    
+    while(status){
+    printf("\nEnter 0 for unsigned, 1 for signed: ");
+    temp = getchar();
+    if(temp==0||temp==1){
+        isSigned=temp;
+        status=SUCCESS;
+        }
+    else 
+        printf("\nERROR. Try again!\n");
+    }
+   
+    status=ERROR;
+    while(status){
+    printf("\nEnter amount of whole digits: ");
+    temp = getchar();
+    if(isdigit(temp)){
+        wholeDigits=temp;
+        status=SUCCESS;
+        }
+    else 
+        printf("\nERROR. Try again!\n");
+    }
 
+    status=ERROR;
+    while(status){
+    printf("\nEnter amount of fractionary digits: ");
+    temp = getchar();
+    if(isdigit(temp)){
+        fractionaryDigits=temp;
+        status=SUCCESS;
+        }
+    else 
+        printf("\nERROR. Try again!\n");
+    }
+
+    
 }
