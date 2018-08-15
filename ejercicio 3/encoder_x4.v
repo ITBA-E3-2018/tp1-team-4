2043398c6095623947bed656063b888f1cd59ccd
lisandro <lialvarez@itba.edu.ar>
2018-08-15 03:06:02 -0300

    Output como 'vector'

--- ejercicio 3/encoder_x4.v
+++ ejercicio 3/encoder_x4.v
@@ -4,19 +4,19 @@
 // Electronica III - Grupo 4            //
 // -------------------------------------//
 
-module encoder_x4(z1, z0, y, x);
+module encoder_x4(z, y, x);
     
     // Inputs-Outputs
     input[3:0]  x;
-    output      z1, z2, y;
-    wire        z1, z2, y;
+    output[1:0] z;
+    output      y;     
 
     // Descripción de nodos internos
-    wire        net1, net2;
+    wire    net1, net2;
     not(net1, x[2]);
     and(net2, net1, x[1]);
 
-    or(z1, x[2], x[3]);
-    or(z0, x[3], net2);
+    or(z[1], x[2], x[3]);
+    or(z[0], x[3], net2);
     or(y, x[0], x[1], x[2], x[3]);
 endmodule
