74cef94a7b5e45de15f2a16d3a83df3058ae921f
lisandro <lialvarez@itba.edu.ar>
2018-08-15 02:47:43 -0300

    Testbench Module

---
+++ ejercicio 3/encoder_testbench.v
@@ -0,0 +1,23 @@
+//------------------------------------------//
+// Test Bench para encoder con              //
+// prioridad de 4 inputs                    //
+// Nombre del archivo: encoder_testbench.v  //
+// Electronica III - Grupo 4                //
+// -----------------------------------------//
+
+`include "encoder_x4.v"
+
+module ecoder_tb;
+    reg[3:0]    x;
+    wire        z1, z0, y;
+    encoder_x4  encoder(.x(x), .y(y), .z1(z1), .z0(z0));
+
+    initial begin
+        #10 x = 4'b0001;
+        #10 x = 4'b0010;
+        #10 x = 4'b0100;
+        #10 x = 4'b1000;
+
+        #10 $stop;
+    end
+endmodule;
