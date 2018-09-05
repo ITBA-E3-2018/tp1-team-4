d3146d2d687cef20c5a4e5ce342eee17a800e4da
matias <mfogg@itba.edu.ar>
2018-09-05 12:08:21 -0300

    chequeo de input

--- ejercicio 1/ejercicio1.c
+++ ejercicio 1/ejercicio1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <math.h>
+#include <string.h>
 #define SUCCESS 0
 #define ERROR 1
 
@@ -14,41 +15,68 @@ int main(){
     float rangeupper=0;
     float rangelower=0;
     float rangemodule=0;
-    
+    char *end;
+    char buf[30];
+    int errorCounter=0;
+
     while(status){
     printf("\nEnter 0 for unsigned, 1 for signed: ");
-    scanf("%d",&temp);
-    printf("%d",temp);
+    do {
+        if (errorCounter!=0) printf("\nERROR. Not an integer! Try again!\n\nEnter 0 for unsigned, 1 for signed:");
+     if (!fgets(buf, sizeof buf, stdin))
+        break;
+        buf[strlen(buf) - 1] = 0;
+        temp = strtol(buf, &end, 10);
+        errorCounter++;
+    } while (end != buf + strlen(buf));
+    errorCounter=0;
     if(temp==0||temp==1){
         isSigned=temp;
         status=SUCCESS;
         }
     else 
-        printf("\nERROR. Try again!\n");
+        printf("\nERROR. Not a valid integer! Try again!\n");
     }
    
     status=ERROR;
     while(status){
     printf("\nEnter amount of whole digits: ");
-    scanf("%d",&temp);
+    do {
+        if (errorCounter!=0) printf("\nERROR. Not an integer! Try again!\n\nEnter amount of whole digits:");
+     if (!fgets(buf, sizeof buf, stdin))
+        break;
+        buf[strlen(buf) - 1] = 0;
+        temp = strtol(buf, &end, 10);
+        errorCounter++;
+    } while (end != buf + strlen(buf));
+    errorCounter=0;
     if(0<=temp&&temp<=1000){
         wholeDigits=temp;
         status=SUCCESS;
         }
     else 
-        printf("\nERROR. Try again!\n");
+        printf("\nERROR. Not a valid integer! Try again!\n");
     }
-
+   
+    //FRACTIONARY
     status=ERROR;
     while(status){
     printf("\nEnter amount of fractionary digits: ");
-    scanf("%d",&temp);
+    do {
+        if (errorCounter!=0) printf("\nERROR. Not an integer! Try again!\n\nEnter amount of fractionary digits:");
+     if (!fgets(buf, sizeof buf, stdin))
+        break;
+        buf[strlen(buf) - 1] = 0;
+        temp = strtol(buf, &end, 10);
+        errorCounter++;
+    } while (end != buf + strlen(buf));
+    errorCounter=0;
     if(0<=temp&&temp<=1000){
         fractionaryDigits=temp;
         status=SUCCESS;
         }
     else 
-        printf("\nERROR. Try again!\n");
+        printf("\nERROR. Not a valid integer! Try again!\n");
     }
 
     resolution=pow(2.00,(-1*(fractionaryDigits)));
@@ -70,5 +98,5 @@ int main(){
     }
 
     printf("When using this convention, the resolution for representable numbers is %f, the range is [%f,%f], and the amount of different numbers is %f.",resolution,rangelower,rangeupper,rangemodule);
-    getchar();
+    
 }
