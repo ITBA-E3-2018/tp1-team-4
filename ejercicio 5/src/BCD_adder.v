42969b009c14fc92fc5a2ec5052e71d7b282e947
Lisandro Alvarez <lialvarez@itba.edu.ar>
2018-09-05 19:51:41 -0300

    correccion BCD_adder

--- ejercicio 5/src/BCD_adder.v
+++ ejercicio 5/src/BCD_adder.v
@@ -1,19 +1,19 @@
-module BCD_adder(in1, in2, Cin, MSout, LSout);
+module BCD_adder(in1, in2, MSout, LSout);
     input[3:0]      in1, in2;
-    input           Cin;
     output [3:0]    LSout;
     output [3:0]    MSout;
-    reg[4:0]        sum;
+    reg[7:0]        sum;
 
     assign LSout = sum[3:0];
-    assign MSout = sum[4];
+    assign MSout = sum[7:4];
 
-    always @(in1, in2, Cin)
+    always @(in1, in2)
     begin
-        sum = in1 + in2 + Cin;
-        if((sum > 9))
+        sum = in1 + in2;
+        if((sum[3:0] > 9))
         begin
-            sum = sum + 6;
+            sum[3:0] = sum[3:0] + 6;
+            sum[7:4] = 1;
         end
     end
 endmodule
--- ejercicio 5/src/BCD_tb.v
+++ ejercicio 5/src/BCD_tb.v
@@ -2,31 +2,30 @@
 
 module BCD_tb;
     reg[3:0]    in1, in2;
-    reg         Cin;
     wire[3:0]   LSout, MSout;
     wire        Cout;
 
-    BCD_adder dut(in1, in2, Cin, MSout, LSout);
+    BCD_adder dut(in1, in2, MSout, LSout);
 
     initial
     begin
-        in1 = 0; in2 = 5; Cin = 0; #10;
-        in1 = 1; in2 = 6; Cin = 0; #10;
-        in1 = 2; in2 = 7; Cin = 0; #10;
-        in1 = 3; in2 = 8; Cin = 0; #10;
-        in1 = 4; in2 = 9; Cin = 0; #10;
-        in1 = 5; in2 = 0; Cin = 0; #10;
-        in1 = 6; in2 = 1; Cin = 0; #10;
-        in1 = 7; in2 = 2; Cin = 0; #10;
-        in1 = 8; in2 = 3; Cin = 0; #10;
-        in1 = 9; in2 = 4; Cin = 0; #10;
-        in1 = 0; in2 = 7; Cin = 0; #10;
-        in1 = 1; in2 = 8; Cin = 0; #10;
-        in1 = 2; in2 = 9; Cin = 0; #10;
+        in1 = 9; in2 = 9;  #10;
+        in1 = 9; in2 = 1;  #10;
+        in1 = 2; in2 = 7;  #10;
+        in1 = 3; in2 = 8;  #10;
+        in1 = 4; in2 = 9;  #10;
+        in1 = 5; in2 = 0;  #10;
+        in1 = 6; in2 = 1;  #10;
+        in1 = 7; in2 = 2;  #10;
+        in1 = 8; in2 = 3;  #10;
+        in1 = 9; in2 = 4;  #10;
+        in1 = 0; in2 = 7;  #10;
+        in1 = 1; in2 = 8;  #10;
+        in1 = 2; in2 = 9;  #10;
     end
 
     initial
-        $monitor("Cin = %b --- in1 = %b(%d) --- in2 = %b(%d) --- out = %b %b(%d%d)",
-                 Cin, in1, in1, in2, in2, MSout, LSout, MSout, LSout);
+        $monitor("in1 = %d --- in2 = %d --- out = %b %b(%d%d)",
+                 in1, in2, MSout, LSout, MSout, LSout);
 
 endmodule
